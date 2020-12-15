847d6fd4c52316c90056c928d2a18fc1fab4400d
Jayaprakash35 <61181823+Jayaprakash35@users.noreply.github.com>
2020-12-14 19:49:52 -0800

    FirstCommit
    First Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+let path = document.querySelectorAll('path');
+
+for (let i = 0; i < path.length; i++) {
+    console.log(`letter ${i} is  ${path[i].getTotalLength()}`);
+}
+
+let anim_h1 = document.querySelector("welcome-section__h1");
+
+window.addEventListener("scroll", () => {
+    if (scrollY = 100) {
+        anim_h1.classList.replace('welcome-section__h1', 'animate-h1')
+    }
+})
+
+*/
+
+
+(function() {
+    $('body').css('visibility', 'visible');
+})();
